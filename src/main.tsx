90fb9ba5b9c6d29407d3049fb834fbb0d39bf318
dexterlgm <31015968+dexterlgm@users.noreply.github.com>
2025-12-26 18:55:48 +0100

    Overriding of some standard bootstrap variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const queryClient = new QueryClient();
 
